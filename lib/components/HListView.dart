92bdd563eaae0e6e703784259eddc5b53a4aec92
gleydsonbrito <gleydsonbrito@hotmail.com>
2020-09-07 10:46:51 -0300

    refactors

--- lib/components/HListView.dart
+++ lib/components/HListView.dart
@@ -4,6 +4,16 @@ import 'package:progesti_app/components/IconOptions.dart';
 class HListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var icons = new List<Widget>();
+    icons = [
+      IconOptions(),
+      IconOptions(),
+      IconOptions(),
+      IconOptions(),
+      IconOptions(),
+      IconOptions()
+    ];
+
     return GestureDetector(
       onTap: () {
         print("Touchable icon");
@@ -13,14 +23,7 @@ class HListView extends StatelessWidget {
         height: 125,
         child: ListView(
           scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            IconOptions(),
-            IconOptions(),
-            IconOptions(),
-            IconOptions(),
-            IconOptions(),
-            IconOptions()
-          ],
+          children: icons,
         ),
       ),
     );
