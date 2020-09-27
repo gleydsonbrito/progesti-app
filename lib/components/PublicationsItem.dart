c03176bf432450719da38d8e24bdc9a56eb94315
gleydsonbrito <gleydsonbrito@hotmail.com>
2020-09-26 23:46:12 -0300

    refactors

--- lib/components/PublicationsItem.dart
+++ lib/components/PublicationsItem.dart
@@ -25,18 +25,3 @@ class PublicationsItem extends StatelessWidget {
     );
   }
 }
-
-var publications = [
-  PublicationsItem(
-    'Retenção e Evasão na Assistência Estudantil',
-    'Este documento representa uma das etapas do processo de avaliação que a Pró-Reitoria de Gestão Estudantil e Inclusão (Progesti) vem realizando sobre as políticas afirmativas de assistência estudantil, no âmbito da Universidade Federal Rural de Pernambuco (UFRPE), na Sede e Unidades Acadêmicas de Garanhuns (UAG), Serra Talhada (UAST) e Cabo de Santo Agostinho (UACSA).',
-    'Retenção e Evasão.pdf',
-    'http://www.progesti.ufrpe.br/sites/www.progesti.ufrpe.br/files/Retencao-Evasao-Progesti.pdf',
-  ),
-  PublicationsItem(
-    'Pesquisa de Satisfação 2017',
-    'A pesquisa foi realizada visando conhecer o perfl e a satisfação dos usuários da política de assistência estudantil para subsidiar a melhoria das ações desenvolvidas, bem como oferecer elementos que permitam que sejam traçadas novas estratégias que visem aprimorar e qualifcar, constantemente, a implementação de programas e projetos de apoio aos estudantes de graduação na instituição.',
-    'Pesquisa de Satisfação 2017.pdf',
-    'http://www.progesti.ufrpe.br/sites/www.progesti.ufrpe.br/files/Pesquisa_Satisfacao_2017.pdf',
-  ),
-];
